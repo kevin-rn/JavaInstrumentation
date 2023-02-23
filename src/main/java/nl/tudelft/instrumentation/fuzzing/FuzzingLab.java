0da54eb15ee46f2a909cb6ce8f8a21018bd1fd71
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2023-02-23 13:14:45 +0000

    Handle different branch conditions

--- src/main/java/nl/tudelft/instrumentation/fuzzing/FuzzingLab.java
+++ src/main/java/nl/tudelft/instrumentation/fuzzing/FuzzingLab.java
@@ -26,50 +26,115 @@ public class FuzzingLab {
          */
         static void encounteredNewBranch(MyVar condition, boolean value, int line_nr) {
                 // do something useful
-
-                int distanceBranch = distanceCounter(condition, value);
+                int distanceBranch = rootDistanceCounter(condition, value);
                 visited.add(line_nr);
                 System.out.println(distanceBranch);
                 totalDistance += distanceBranch;
 
         }
 
-        static int distanceCounter(MyVar condition, boolean value) {
-                int count = 0;
-
+        /**
+         * Method for calculating the top-level layer (root) of the branch condition.
+         * @param condition Branch condition as MyVar type.
+         * @param value Boolean value of the branch condition.
+         * @return Branch condition distance.
+         */
+        static int rootDistanceCounter(MyVar condition, boolean value) {
                 switch(condition.type) {
                         case BOOL:
-                                count = value ? 0 : 1;
-                                break;
-                        case INT:
-                                count = condition.int_value;
-                                break;
-                        // case STRING:
-                        //         String strCondition = condition.str_value;
-                                
-
-                        //         break;
+                                return value ? 0 : 1;
                         case UNARY:
-                                int tempDist = distanceCounter(condition.left, value);
-                                count = unaryDistance(condition, tempDist);
+                                int tempDist = nodeDistanceCounter(condition.left, value, "");
+                                if (condition.operator.equals("!")) {
+                                        return normalizeDistance(1 - tempDist);
+                                } 
                                 break;
-                        // case BINARY:
-                                
+                        case BINARY:
+                                String str1 = "", str2 = "";
+                                int expr1 = nodeDistanceCounter(condition.left, value, str1);
+                                int expr2 = nodeDistanceCounter(condition.right, value, str2);
+                                return binaryDistance(condition, expr1, expr2, str1, str2);                                
                         default:
                                 System.out.printf("Error: Encountered unexpected Branch condition type %s%n", condition.type);
+                                break;
                                
                 }
-
-                return count;
+                return Integer.MIN_VALUE;
         }
 
-        // Handle ! operator
-        static int unaryDistance(MyVar condition, int dist) {
-                if (condition.operator.equals('!')) {
-                        return normalizeDistance(1 - dist)
-                } else {
-                        return Integer.MIN_VALUE;
+        /**
+         * Method for calculating the node level branch conditions.
+         * @param condition Branch condition as MyVar type.
+         * @param value Boolean value of the branch condition.
+         * @param str String value of the branch condition.
+         * @return Branch condition distance.
+         */
+        static int nodeDistanceCounter(MyVar condition, boolean value, String str) {
+                switch(condition.type) {
+                        case BOOL:
+                                return value ? 0 : 1;
+                        case INT:
+                                return condition.int_value;
+                        case STRING:
+                                str = condition.str_value;
+                                return Integer.MIN_VALUE;
+                        case UNARY:
+                        case BINARY:
+                                return rootDistanceCounter(condition, value) // Reuse method for calculating unary and binary conditions
+                        default:
+                                System.out.printf("Error: Encountered unexpected Branch condition type %s%n", condition.type);
+                                return Integer.MIN_VALUE;
                 }
+        }
+
+        /**
+         * Method for calculating the binary type branch conditions.
+         * @param condition Branch condition as MyVar type.
+         * @param expr1 Left-hand expression from the operator of the conditon.
+         * @param expr2 Right-hand expression from the operator of the conditon.
+         * @param str1 Left-hand string expression.
+         * @param str2 Right-hand string expression.
+         * @return Branch condition distance.
+         */
+        static int binaryDistance(MyVar condition, int expr1, int expr2, String str1, String str2) {
+                int dist = Integer.MIN_VALUE;
+                switch(condition.operator){
+                        case "||": 
+                                dist = Math.min(expr1, expr2);
+                                return normalizeDistance(dist);
+                        case "&&":
+                                return normalizeDistance(expr1 + expr2);
+                        case "==": 
+                                if(condition.type == TypeEnum.STRING || !str1.isEmpty() || !str2.isEmpty()) {
+                                        int dst = editDistDP(str1, str2);
+                                        return normalizeDistance(dst);
+                                } else {
+                                        return normalizeDistance(Math.abs(expr1 - expr2));
+                                }
+                        case "!=": 
+                                if(condition.type == TypeEnum.STRING) {
+                                        dist = !str1.equals(str2) ? 0 : 1;
+                                } else {
+                                        dist = (expr1 != expr2) ? 0 : 1;
+                                }
+                                return normalizeDistance(dist);
+                        case "<": 
+                                dist = (expr1 < expr2) ? 0 : expr1 - expr2 + 1;
+                                return normalizeDistance(dist);
+                        case "<=": 
+                                dist = (expr1 <= expr2) ? 0 : expr1 - expr2;
+                                return normalizeDistance(dist);
+                        case ">":
+                                dist = (expr1 > expr2) ? 0 : expr2 - expr1 + 1;
+                                return normalizeDistance(dist);
+                        case ">=":
+                                dist = (expr1 > expr2) ? 0 : expr2 - expr1;
+                                return normalizeDistance(dist);
+                        default:
+                                System.err.printf("Error: Encountered unexpected binary branch condition type %s%n " + condition.operator);
+                                break;
+                }
+                return dist;
         }
 
         /**
@@ -81,8 +146,13 @@ public class FuzzingLab {
                 return d/(d+1);
         }
 
-        // Levenshtein distance (slide 57 lecture 2)
-        static float editDistDP(String str1, String str2){
+        /**
+         * Compute the edit distance between two strings according to the Levenshtein Distance metric.
+         * @param str1 First string to compute from.
+         * @param str2 Second string to compute from.
+         * @return Distance between two strings.
+         */
+        static int editDistDP(String str1, String str2){
                 int m = str1.length(), n = str2.length();
                 int[][] table = new int[m + 1][n + 1];
 
