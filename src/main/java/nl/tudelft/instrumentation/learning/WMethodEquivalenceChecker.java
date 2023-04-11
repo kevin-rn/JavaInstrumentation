f4b4fb8debb9634465c72fc68d6f023eb93f429c
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-04-11 18:59:07 +0200

    reimplement Wmethod

--- src/main/java/nl/tudelft/instrumentation/learning/WMethodEquivalenceChecker.java
+++ src/main/java/nl/tudelft/instrumentation/learning/WMethodEquivalenceChecker.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class WMethodEquivalenceChecker extends EquivalenceChecker{
+public class WMethodEquivalenceChecker extends EquivalenceChecker {
 
     private int w;
     private AccessSequenceGenerator accessSequenceGenerator;
@@ -14,8 +14,8 @@ public class WMethodEquivalenceChecker extends EquivalenceChecker{
     public WMethodEquivalenceChecker(SystemUnderLearn sul, String[] inputSymbols, int w, DistinguishingSequenceGenerator dg, AccessSequenceGenerator ag) {
         super(sul, inputSymbols);
         this.w = w;
-        this.distinguishingSequenceGenerator= dg;
-        this.accessSequenceGenerator= ag;
+        this.distinguishingSequenceGenerator = dg;
+        this.accessSequenceGenerator = ag;
     }
 
     private static void generateInputSequences(String[] inputs, int W, String current, List<Word<String>> inputSequences) {
@@ -31,41 +31,42 @@ public class WMethodEquivalenceChecker extends EquivalenceChecker{
         }
     }
 
+
     @Override
     public Optional<Word<String>> verify(MealyMachine hypothesis) {
         List<Word<String>> accessSequences = accessSequenceGenerator.getAccessSequences();
         List<Word<String>> distinguishingSequences = distinguishingSequenceGenerator.getDistinguishingSequences();
 
-        // Generate all possible combinations of length w from the inputsymbols.
         List<Word<String>> inputSequences = new ArrayList<>();
         generateInputSequences(inputSymbols, w, "", inputSequences);
 
         // Iterate over all access sequences
         for (Word<String> accessSequence : accessSequences) {
-
-            // Iterate over all input sequences of length w
-            for(Word<String> input : inputSequences ) {
-                Word<String> temp = accessSequence.append(input);
+            // Iterate over all words of length w over the input symbols
+            for (Word<String> word : inputSequences) {
+                // Build the test input
+                Word<String> testInput = accessSequence.append(word);
 
                 // Iterate over all distinguishing sequences
                 for (Word<String> distinguishingSequence : distinguishingSequences) {
-                    // Build the test input
-                    Word<String> testInput = temp.append(distinguishingSequence);
+                    // Extend the test input with the distinguishing sequence
+                    Word<String> extendedTestInput = testInput.append(distinguishingSequence);
 
-                    // For the test input, generate model and real output
-                    String[] modelOutput = hypothesis.getOutput(testInput);
-                    String[] realOutput = sul.getOutput(testInput);
+                    // Evaluate the hypothesis and the system under learning on the test input
+                    Word<String> hypothesisOutput = new Word<String>(hypothesis.getOutput(testInput));
+                    Word<String> systemOutput = new Word<String>(sul.getLastOutput(testInput));
 
-                    // If these are not equal a counterexample has been found and returned.
-                    if(!Arrays.equals(modelOutput, realOutput)) {
-                        return Optional.of(testInput);
+                    // If the hypothesis and the system under learning disagree on the output, a counterexample has been found
+                    if (!hypothesisOutput.equals(systemOutput)) {
+                        return Optional.of(extendedTestInput);
                     }
                 }
             }
         }
 
-        // no counterexample found
+        // No counterexample found
         return Optional.empty();
     }
+
 
 }
